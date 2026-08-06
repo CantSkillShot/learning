47733c516b73ba3ee25483536d35bf2af8310ffd
Jason Morris <jasonmorris.gamedesign@outlook.com>
2026-08-06 16:21:17 -0400

    Adding movement
    Added basic movement for player to move side to side as well as a jump. Created methods for handling the movement, input, and jumping.

--- Learning/Assets/Player.cs
+++ Learning/Assets/Player.cs
@@ -2,10 +2,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-    void Update()
+    [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8;
+    private float xInput;
+
+    private void Awake()
     {
-        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal"), rb.linearVelocityY);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        HandleInput();
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 }
